2731e92246d01f4cb413609191a2ef3dc4a5e4dc
andrewperez <andrewprz5@gmail.com>
2025-06-16 08:12:45 -0400

    Initiate Responsive Layout

--- src/index.js
+++ src/index.js
@@ -51,12 +51,105 @@ function goIntoContent() {
     }
 };
 
+function updateNavMenu() {
+    const navMenu = document.getElementById("nav-menu");
+    const 
+        if(window.innerWidth <= 768) {
+        navMenu.innerHTML = `
+        <div class="menu-toggle-container">
+            <button class="menu-toggle-btn" id="menu-toggle">
+                <span class="svg-container">
+                    <svg class="svg-logo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512">
+                        <rect class="svg-piece" width="512" height="102"></rect>
+                        <rect class="svg-piece" y="205" width="512" height="102"></rect>
+                        <rect class="svg-piece" y="410" width="512" height="102"></rect>
+                    </svg>
+                </span>
+            </button>
+        </div>
+        `;
+
+        const menuToggleBtn = document.getElementById("menu-toggle");
+        if (menuToggleBtn) {
+        menuToggleBtn.addEventListener("click", () => {
+        menuToggleBtn.classList.toggle("active");
+        
+        
+        });
+    }
+        
+        } else {
+        navMenu.innerHTML = `
+                <ul>
+                    <li class="list active" id="home-list">
+                        <button class="tab-button active" id="home">Home</button>
+                    </li>
+                     <li class="list active"  id="about-list">
+                        <button class="tab-button active" id="about">About</button>
+                    </li>
+                     <li class="list active" id="menu-list">
+                        <div>
+                            <button class ="tab-button active" id="menu">Menu</button>
+                            <span></span>
+                        </div>
+                        <ul class="submenu">
+                            <li class="subcategory">
+                                <a href="/menu#appetizers">Appetizers</a>
+                            </li>
+                            <li class="subcategory">
+                                <a href="/menu#soup_and_salad">Soup and Salad</a>
+                            </li>
+                            <li class="subcategory">
+                                <a href="/menu#burgers">Burgers</a>
+                            </li>
+                            <li class="subcategory">
+                                <a href="/menu#sandwiches">Sandwiches</a>
+                            </li>
+                            <li class="subcategory">
+                                <a href="/menu#specialty_sandwiches">Specialty Sandwiches</a>
+                            </li>
+                            <li class="subcategory">
+                                <a href="/menu#meats">Meats</a>
+                            </li>
+                            <li class="subcategory">
+                                <a href="/menu#fish">Fish</a>   
+                            </li>
+                            <li class="subcategory">
+                                <a href="/menu#italian">Italian</a>
+                            </li>
+                            <li class="subcategory">
+                                <a href="/menu#sides">Sides</a>
+                            </li>
+                            <li class="subcategory">
+                                <a href="/menu#pizza">Pizza</a>
+                            </li>
+                            <li class="subcategory">
+                                <a href="/menu#specialty_pie">Specialty Pie</a>
+                            </li>
+                            <li class="subcategory">
+                                <a href="/menu#gluten_free">Gluten Free</a>
+                            </li>
+                            <li class="subcategory">
+                                <a href="/menu#kids">Kids</a>
+                            </li>
+                        </ul>
+                    </li>
+                     <li class="list active" id="find-us-list">
+                        <button class="tab-button active" id="find-us">Find Us</button>
+                    </li>
+                </ul>
+        `
+        }
+};
+
 home(content);
 switchTab(content);
 
 window.addEventListener("DOMContentLoaded", () => {
     const tabs = document.querySelectorAll('.tab-button');
     const listItems = document.querySelectorAll('.list');
+    const submenu = document.querySelector('.submenu');
+    const menuList = document.getElementById("menu-list");
 
     function activeTab(tabId, listId) {
         tabs.forEach(btn => btn.classList.remove('active'));
@@ -66,10 +159,21 @@ window.addEventListener("DOMContentLoaded", () => {
         document.getElementById(listId).classList.add('active');
     }
    
-
     tabs.forEach(btn => {
         btn.addEventListener('click', () => activeTab(btn.id, listItems[Array.from(tabs).indexOf(btn)].id));
     });
+
+    menuList.addEventListener('mouseenter', () => {
+        submenu.classList.add('focus');
+    });
+
+    menuList.addEventListener('mouseleave', () => {
+        submenu.classList.remove('focus');
+    });
     
+
+    updateNavMenu();
     activeTab("home", "home-list");
 });
+
+window.addEventListener('resize', updateNavMenu);
