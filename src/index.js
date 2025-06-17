40bf868f789a35e261f02e3af5d42d1a1c1ffa70
andrewperez <andrewprz5@gmail.com>
2025-06-17 01:03:39 -0400

    Fix Mobile Interface

--- src/index.js
+++ src/index.js
@@ -54,173 +54,43 @@ function goIntoContent() {
 function updateNavMenu() {
     const navMenu = document.getElementById("nav-menu");
         if(window.innerWidth <= 768) {
-        navMenu.innerHTML = `
-        <div class="menu-toggle-container">
-            <button class="menu-toggle-btn" id="menu-toggle">
-                <span class="svg-container">
-                    <svg class="svg-logo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512">
-                        <rect class="svg-piece" width="512" height="102"></rect>
-                        <rect class="svg-piece" y="205" width="512" height="102"></rect>
-                        <rect class="svg-piece" y="410" width="512" height="102"></rect>
-                    </svg>
-                </span>
-            </button>
-        </div>
-        `;
-
-        const menuToggleBtn = document.getElementById("menu-toggle");
-        if (menuToggleBtn) {
-        menuToggleBtn.addEventListener("click", () => {
-        menuToggleBtn.classList.toggle("active");
-        
-        const existing = document.querySelector(".mobile-menu");
-        if (existing) {
-            existing.remove();
-        } else {
-            const newFlexItem = document.createElement("div");
-            newFlexItem.classList.add('flex-item', 'mobile-menu');
-            newFlexItem.innerHTML = `
-            <div id="mobile-menu">    
-                <ul>
-                    <li>
-                        <a href="https://nelliesplacenj.com/">Home</a>
-                    </li>
-                    <li>
-                        <a href="https://nelliesplacenj.com/about-nellies-place/">About</a>
-                    </li>
-                    <li id="mobile-menu-list">
-                        <div class="mobile-submenu-container">
-                            <a href="https://nelliesplacenj.com/menu/" id="mobile-menu-link"aria-haspopup="true">Menu</a>
-                            <span></span>
-                        </div>
-                        <ul class="submenu mobile">
-                            <li class="subcategory">
-                                <a href="/menu#appetizers">Appetizers</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#soup_and_salad">Soup and Salad</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#burgers">Burgers</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#sandwiches">Sandwiches</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#specialty_sandwiches">Specialty Sandwiches</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#meats">Meats</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#fish">Fish</a>   
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#italian">Italian</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#sides">Sides</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#pizza">Pizza</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#specialty_pie">Specialty Pie</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#gluten_free">Gluten Free</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#kids">Kids</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="https://nelliesplacenj.com/find-us/">Find Us</a>
-                    </li>
-                </ul>
+            navMenu.innerHTML = `
+            <div class="menu-toggle-container">
+                <button class="menu-toggle-btn" id="menu-toggle">
+                    <span class="svg-container">
+                        <svg class="svg-logo" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512">
+                            <rect class="svg-piece" width="512" height="102"></rect>
+                            <rect class="svg-piece" y="205" width="512" height="102"></rect>
+                            <rect class="svg-piece" y="410" width="512" height="102"></rect>
+                        </svg>
+                    </span>
+                </button>
             </div>
             `;
 
-            const flexItems = document.querySelectorAll('.flex-item');
-
-            if (flexItems.length >= 2) {
-                flexItems[2].parentNode.insertBefore(newFlexItem, flexItems[2]);
-            } else {
-                const nav = document.querySelector('nav');
-                nav.appendChild(newFlexItem);
-            }
+            const menuToggleBtn = document.getElementById("menu-toggle");
+            if (menuToggleBtn) {
+                menuToggleBtn.addEventListener("click", () => {
+                menuToggleBtn.classList.toggle("active");
+                
+                const menu = document.querySelector(".mobile-menu");
+                if (menu) {
+                    menu.classList.toggle('open');
+                }
+                
+            
+            });
         }
         
-        });
-    }
-        
-        } else {
-        navMenu.innerHTML = `
-                <ul>
-                    <li class="list active" id="home-list">
-                        <button class="tab-button active" id="home">Home</button>
-                    </li>
-                     <li class="list active"  id="about-list">
-                        <button class="tab-button active" id="about">About</button>
-                    </li>
-                     <li class="list active" id="menu-list">
-                        <div>
-                            <button class ="tab-button active" id="menu">Menu</button>
-                            <span></span>
-                        </div>
-                        <ul class="submenu">
-                            <li class="subcategory">
-                                <a href="/menu#appetizers">Appetizers</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#soup_and_salad">Soup and Salad</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#burgers">Burgers</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#sandwiches">Sandwiches</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#specialty_sandwiches">Specialty Sandwiches</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#meats">Meats</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#fish">Fish</a>   
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#italian">Italian</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#sides">Sides</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#pizza">Pizza</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#specialty_pie">Specialty Pie</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#gluten_free">Gluten Free</a>
-                            </li>
-                            <li class="subcategory">
-                                <a href="/menu#kids">Kids</a>
-                            </li>
-                        </ul>
-                    </li>
-                     <li class="list active" id="find-us-list">
-                        <button class="tab-button active" id="find-us">Find Us</button>
-                    </li>
-                </ul>
-        `
-        const existing = document.querySelector(".mobile-menu");
-        if (existing) {
-            existing.remove();
+     } else {
+        if (navMenu.dataset.originalHtml) {
+            navMenu.innerHTML = navMenu.dataset.originalHtml;
         }
-     } 
+        const mobileMenu = document.querySelector('.mobile-menu');
+        if (mobileMenu) {
+            mobileMenu.remove();
+        }
+     }
 };
 
 home(content);
